848d409e0c6255ef865fcfd652e98c63d6d6f458
Xu, Yuncheng | CNTD <yuncheng.xu@rakuten.com>
2023-10-16 08:16:13 +0800

    Update 260

--- Algorithms/260.py
+++ Algorithms/260.py
@@ -1,6 +1,10 @@
+from collections import Counter
+
+
 class Solution:
-    def singleNumber(self, nums):
-        note=set()
+    # use set
+    def singleNumber1(self, nums):
+        note = set()
         for num in nums:
             if num not in note:
                 note.add(num)
@@ -8,6 +12,15 @@ class Solution:
                 note.remove(num)
         return list(note)
 
+    # use built-in counter
+    def singleNumber(self, nums):
+        note = Counter(nums)
+        result = []
+        for item in note:
+            if note[item] == 1:
+                result.append(item)
+        return result
 
-solution=Solution()
-print(solution.singleNumber([1,2,1,3,2,5]))
+
+solution = Solution()
+print(solution.singleNumber([1, 2, 1, 3, 2, 5]))
